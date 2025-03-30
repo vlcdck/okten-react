7f1f44fcc3d574e4091bb4434f2176e0b2b3d320
vlcdck <pridatchenkovladislav@gmail.com>
2025-03-30 16:43:46 +0300

    Complete Lesson 2 Task 4

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import './App.css'
 import {TodosComponent} from "./components/todos-component/TodosComponent.tsx";
-// import {CommentsComponent} from "./components/comments-component/CommentsComponent.tsx";
-// import {PostsComponent} from "./components/posts-component/PostsComponent.tsx";
+import {CommentsComponent} from "./components/comments-component/CommentsComponent.tsx";
+import {PostsComponent} from "./components/posts-component/PostsComponent.tsx";
 
 function App() {
 
     return (
         <>
             <TodosComponent/>
+            <CommentsComponent/>
+            <PostsComponent/>
         </>
     )
 }
